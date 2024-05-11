5f906d7bbf85960adbca85b105349b78e69e8cfb
Walter <lilwaltmus@gmail.com>
2024-05-11 21:49:06 +0200

    add clean_move_dir_names to samwise

--- samwise/file_namer.py
+++ samwise/file_namer.py
@@ -9,29 +9,27 @@ def _get_movies_in_dir(dir):
     filenames = os.listdir(dir)
     return [filename for filename in filenames if filename.endswith((".mp4", ".mkv", ".avi"))]
 
+def _build_json_object_for_rename_response(response):
+    response_lines = response.split("\n")
+    return [_build_json_object_for_rename_response_line(response_line) for response_line in response_lines]
+
+def _build_json_object_for_rename_response_line(response_line):
+    old_name, new_name = response_line.split("||")
+    return {"old": old_name, "new": new_name}
+    
+
 def _build_clean_movie_names_in_dir_prompt(filenames):
     prompt = """
-    Below are file names. Clean them up so that they are in the format of '<Movie Title: Subtitle (Year)>'. Put each one in a json object so that your output is a json list of json
-    objects with the fields 'old' and 'new' Here is an example:
+    Below are file names. Clean them up so that they are in the format of '<Movie Title: Subtitle (Year)>'. Put each 
+    on a line with <old-name>||<new-name> Here is an example:
     For the filenames:
     2004 - Survive Style 5+.mkv 
     Akira (1988) 2160p HDR 5.1 Eng - Jpn x265 10bit Phun Psyz.mkv
     Europa.Europa.1990.1080p.BluRay.x264-[YTS.LT].mp4
     You answer should be:
-    [
-        {
-            "old": "2004 - Survive Style 5+.mkv",
-            "new": "Survive Style 5+ (2004).mkv"
-        },
-        {
-            "old": "Akira (1988) 2160p HDR 5.1 Eng - Jpn x265 10bit Phun Psyz.mkv",
-            "new": "Akira (1988).mkv"
-        },
-        {
-            "old": "Europa.Europa.1990.1080p.BluRay.x264-[YTS.LT].mp4",
-            "new": "Europa Europa (1990).mp4"
-        }
-    ]
+    2004 - Survive Style 5+.mkv||Survive Style 5+ (2004).mkv
+    Akira (1988) 2160p HDR 5.1 Eng - Jpn x265 10bit Phun Psyz.mkv||Akira (1988).mkv
+    Europa.Europa.1990.1080p.BluRay.x264-[YTS.LT].mp4||Europa Europa (1990).mp4
     Here are the file name(s) to clean up: 
     """
     for filename in filenames:
@@ -44,15 +42,16 @@ def clean_movie_names_in_dir(dir):
     genai.configure(api_key="AIzaSyBycGic-Hi1hlDHFl62Gs65W3gijIiYb5k")
     text_model = genai.GenerativeModel('gemini-pro')
 
-    print('* Finding all the movies in the directory')
+    print(f"* Finding all the movies in the directory: {dir}")
     movie_titles = _get_movies_in_dir(dir)    
     
     print('* Cleaning up movie names')
     prompt = _build_clean_movie_names_in_dir_prompt(movie_titles)
     
     response = text_model.generate_content(prompt)
-    clean_titles = json.loads(response.text)
+    clean_titles = _build_json_object_for_rename_response(response.text)
     print('* Renaming movies')
     for clean_title in clean_titles:
         _rename_file(os.path.join(dir, clean_title['old']), os.path.join(dir, clean_title['new']))
-        print(f"- {clean_title['old']} --> {clean_title['new']}")
+        print(f"\t- {clean_title['old']} --> {clean_title['new']}")
+    print('* Done :-)')    
--- samwise/samwise.py
+++ samwise/samwise.py
@@ -1,5 +1,6 @@
 import argparse
 from file_mover import move_subtitles, move_movie, remove_source_dir
+from file_namer import clean_movie_names_in_dir
 
 
 def move_movie_downloads(source_dirs):
@@ -13,13 +14,16 @@ def move_movie_downloads(source_dirs):
 def main(source_dirs, action):
     if action == "move_movie_downloads" or action == "mmd":
         move_movie_downloads(source_dirs)
+    elif action == "clean_movie_dir_names" or action == "cmdn":
+        for source_dir in source_dirs:
+            clean_movie_names_in_dir(source_dir)    
     else:
         print("Invalid action")
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='Move movie and subtitles to the correct directories')
-    parser.add_argument('action', type=str, default='move', help='The action to perform[move_movie_downloads(mmd)]')
+    parser.add_argument('action', type=str, default='move', help='The action to perform[move_movie_downloads(mmd), clean_movie_dir_names(cmdn)]')
     parser.add_argument('source_dirs', type=str, nargs='+', help='The source directory(s) to process')
 
     args = parser.parse_args()
