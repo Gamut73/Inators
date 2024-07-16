c2314e9764914039af14d45dcdbd81cdedf8b740
Walter <lilwaltmus@gmail.com>
2024-07-16 20:10:37 +0200

    Clipinator: add option to clip with subtitles

--- clipinator/clipinator.py
+++ clipinator/clipinator.py
@@ -1,18 +1,33 @@
-from moviepy.editor import *
 import argparse
 import os
 
+from moviepy.editor import *
+from moviepy.video.tools.subtitles import SubtitlesClip
+
 CLIP_GROUP_PREFIX = "_X__"
 
-def clip_video(input_file_path, clip_name, start_time, end_time, output_dir):
+
+def clip_video(input_file_path, clip_name, start_time, end_time, output_dir, subtitles_file_path):
     if os.path.exists(output_dir) == False:
         os.makedirs(output_dir)
 
     og_vid_name = os.path.basename(input_file_path)
     clip_save_file_path = _build_clip_file_path(clip_name, og_vid_name, output_dir)
+
     clip = VideoFileClip(input_file_path)
+    if subtitles_file_path != '':
+        clip = _add_subtitiles(clip, subtitles_file_path)
+
     clip = clip.subclip(_convert_time_to_seconds(start_time), _convert_time_to_seconds(end_time))
     clip.write_videofile(clip_save_file_path)
+
+
+def _add_subtitiles(video_clip, subtitles_file_path):
+    generator = lambda txt: TextClip(txt, font='Arial', fontsize=50, color='white', method='caption', align='South', size=video_clip.size)
+
+    subtitle_clip = SubtitlesClip(subtitles_file_path, generator)
+    return CompositeVideoClip((video_clip, subtitle_clip.set_position(('center', 'bottom'))), size=video_clip.size)
+
 
 
 def _build_clip_file_path(clip_name, og_vid_name, output_dir):
@@ -32,18 +47,23 @@ def _build_clip_file_path(clip_name, og_vid_name, output_dir):
 def _convert_time_to_seconds(time):
     while (time.count(":") != 2):
         time = "00:" + time
-        
+
     time_split = time.split(":")
-    return int(time_split[0])*3600 + int(time_split[1])*60 + int(time_split[2])    
-    
-    
+    return int(time_split[0]) * 3600 + int(time_split[1]) * 60 + int(time_split[2])
+
+
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser(description= 'Clip a video')
+    default_output_dir = os.path.join(os.path.expanduser("~"), "Videos", "Clips")
+
+    parser = argparse.ArgumentParser(description='Clip a video')
     parser.add_argument('input_file_path', help="Name of the input file")
-    parser.add_argument('start_time', help="Start time of the clip in the format hh:mm:ss (e.g 10 is ten seconds, 00:10 is ten seconds, 21:00 is twenty one minutes, 00:21:00 is also twenty one minutes)")
-    parser.add_argument('end_time', help="End time of the clip in the same format as start time")        
+    parser.add_argument('start_time',
+                        help="Start time of the clip in the format hh:mm:ss (e.g 10 is ten seconds, 00:10 is ten seconds, 21:00 is twenty one minutes, 00:21:00 is also twenty one minutes)")
+    parser.add_argument('end_time', help="End time of the clip in the same format as start time")
     parser.add_argument('clip_name', help="Name of the clip")
-    parser.add_argument('-o', '--output_dir', default='~/Videos/Clips', help="Output directory. Creates a new directory if it doesn't exist. Default is the current directory")
-    
+    parser.add_argument('-o', '--output_dir', default=default_output_dir,
+                        help="Output directory. Creates a new directory if it doesn't exist. Default is the current directory")
+    parser.add_argument('-s', '--subtitles', default='', help="Path to the subtitles file (.srt)")
+
     args = parser.parse_args()
-    clip_video(args.input_file_path, args.clip_name, args.start_time, args.end_time, args.output_dir)
+    clip_video(args.input_file_path, args.clip_name, args.start_time, args.end_time, args.output_dir, args.subtitles)
