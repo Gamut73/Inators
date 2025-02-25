4b5ddee94ff7be9936ecb08dcc61594e645fa355
Walter <lilwaltmus@gmail.com>
2025-02-25 20:55:34 +0200

    Rename the actions to be more verbose

--- util/file_namer.py
+++ util/file_namer.py
@@ -13,8 +13,9 @@ def _get_movies_in_dir(directory):
     return [os.path.join(directory, filename) for filename in filenames if filename.endswith((".mp4", ".mkv", ".avi"))]
 
 def _build_json_object_for_rename_response(response):
-    response_lines = response.split("\n")
+    response_lines  = response.splitlines()
     return [_build_json_object_for_rename_response_line(response_line) for response_line in response_lines]
+
 
 def _build_json_object_for_rename_response_line(response_line):
     old_name, new_name = response_line.split("||")
@@ -45,25 +46,23 @@ def get_cleaned_names_for_movie_files(movie_names):
 
     gemini_api_key = os.getenv('GEMINI_API_KEY')
     genai.configure(api_key=gemini_api_key)
-    text_model = genai.GenerativeModel('gemini-pro')
+    text_model = genai.GenerativeModel('gemini-2.0-flash')
 
     prompt = _build_clean_movie_names_in_dir_prompt(movie_names)
 
     response = text_model.generate_content(prompt)
-    clean_titles = _build_json_object_for_rename_response(response.text)
+    clean_titles = _build_json_object_for_rename_response(response.text.rstrip())
 
     return clean_titles
 
 
 def clean_list_of_movie_files(movie_files):
-    print('* Cleaning up movie names')
     clean_titles = get_cleaned_names_for_movie_files(movie_files)
 
-    print('* Renaming movies')
     for clean_title in clean_titles:
         new_filename = os.path.join(os.path.dirname(clean_title['old']), clean_title['new'])
         _rename_file(clean_title['old'], new_filename)
-        print(f"\t- {clean_title['old']} --> {clean_title['new']}")
+        print(f"- {clean_title['old']} --> {clean_title['new']}")
     print('* Done :-)')
 
 
@@ -72,6 +71,10 @@ def clean_movie_names_in_dir(directory):
     movie_files = _get_movies_in_dir(directory)
 
     clean_list_of_movie_files(movie_files)
+
+
+def clean_movie_name(filepath):
+    clean_list_of_movie_files([filepath])
 
 
 def _load_dotenv():
